114ab4a1d83b04fad199aa000fd941738c01065d
Tyler <nelson.tyler.g@gmail.com>
2022-10-11 17:34:37 -0500

    fixed some random stuff, working on making boss now

--- boss.py
+++ boss.py
@@ -1,8 +1,23 @@
 from entity import entity
+from math import atan
+import pygame
 
 class boss(entity):
 
     def __init__(self):
-        self.pos = (0,0)
-        self.dir = "none"
+        self.posX = 0
+        self.posY = 0
+        self.hue = pygame.Color(0,255,0)
+        self.dir = 0
         self.name = "boss"
+        self.speed = 2
+
+    def isAtCenter(self):
+        if self.posX - 480 > self.speed and self.posY - 270 > self.speed:
+            self.dir = atan((self.posY - 270)/(self.posX - 480))
+            if (self.posY - 270)*(self.posX - 480) < 0:
+                self.dir = -atan((self.posY - 270)/(self.posX - 480))
+        else:
+            self.posX = 480
+            self.poxY = 270
+            return True
